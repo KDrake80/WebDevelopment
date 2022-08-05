bd4b4382c0e4739fea4eeff5e9f6882e69a41084
KDrake80 <kevindrake80@gmail.com>
2022-08-05 08:45:13 -0600

    Completed
    Final Update

--- Assignment 3/src/javascript/components/Article.js
+++ Assignment 3/src/javascript/components/Article.js
@@ -1,5 +1,5 @@
-import React from 'react'
-
+import React from 'react';
+import MyImage from 'C:/Users/kevin/ReactProjects/react1/src/ReactSS.png'
 const Article = () => {
   return (
     <article>
@@ -13,11 +13,22 @@ const Article = () => {
         </ol>
         <ol>
             <h1>CSS File</h1>
+            <h4>(Main CSS File)</h4>
             <li><h3>index.css</h3></li>
         </ol>
         <ol>
         <h1>Other Files</h1>
+        <h4>(Main File)</h4>
+          <li><h3>Index.js</h3></li>
+          <h4>(Image File)</h4>
+          <li><h3>ReactSS.png</h3></li>
         </ol>
+        <img className='image'
+        src={MyImage} 
+        alt="Invalid"
+        height={500}
+        width={500}
+        ></img>
     </article>
   )
 }
--- Assignment 3/src/javascript/components/Section.js
+++ Assignment 3/src/javascript/components/Section.js
@@ -4,9 +4,9 @@ const Section = () => {
   return (
     <nav>
         <ul>
-            <li><a href='https://github.com/KDrake80/WebDevelopment/blob/main/Assignment%203/src/components/Article.js'>JavaScript Files</a></li>
-            <li><a href='#'>CSS File</a></li>
-            <li><a href='#'>Other Files</a></li>
+            <li><a href='https://github.com/KDrake80/WebDevelopment/tree/main/Assignment%203/src/javascript'>JavaScript Files</a></li>
+            <li><a href='https://github.com/KDrake80/WebDevelopment/tree/main/Assignment%203/src/css'>CSS File</a></li>
+            <li><a href='https://github.com/KDrake80/WebDevelopment/blob/main/Assignment%203/src/index.js'>Other Files</a></li>
         </ul>
     </nav>
   )
